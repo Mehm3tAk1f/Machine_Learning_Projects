531a08b3ef284684faa35190e0053017c904e446
Mehmet_Akif <ufacikmehmetakif8@gmail.com>
2025-07-10 23:51:16 +0300

    Added more details

---
+++ recommendation-system/test.py
@@ -0,0 +1,4 @@
+list = [{'id': 657, 'name': 'fire'}, {'id': 380, 'name': 'sibling relationship'}, {'id': 3030, 'name': 'nightmare'}, {'id': 1964, 'name': 'cave'}, {'id': 2669, 'name': 'motel'}, {'id': 7312, 'name': 'road trip'}, {'id': 9720, 'name': 'woods'}, {'id': 9714, 'name': 'remake'}, {'id': 187056, 'name': 'woman director'}]
+
+
+print([kw['name'] for kw in list])
